fea317488ee38cccfb36c7946d55191fec2d104c
Phi Tran <phitrann0810@gmail.com>
2026-03-24 23:10:02 -0500

    pictures

--- backend/routes/exhibits.js
+++ backend/routes/exhibits.js
@@ -8,7 +8,7 @@ const path = require('path');
 const fs = require('fs');
 
 // Ensure image directory exists
-const imageDir = path.join(__dirname, '../../frontend/src/assets/images/Exhibits_Images');
+const imageDir = path.join(__dirname, '../uploads/Exhibits_Images');
 if (!fs.existsSync(imageDir)) {
     fs.mkdirSync(imageDir, { recursive: true });
 }
